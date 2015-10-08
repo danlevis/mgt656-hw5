a681257ff1780dea96389e8cd0b151e1126c8b75
danlevis <dan.levis@yale.edu>
2015-10-08 21:46:40 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,12 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/netid', function(request, response){
+    response.send('YOUR SHOULD PUT YOUR NETID HERE!!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('brave-eagle');
+});
+
 app.listen(process.env.PORT || 4000);
